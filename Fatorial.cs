4c76f4ee2102cfa73300975064e3d9db26bdfdab
sp-andlo <sp-andlo@SPOCPD05>
2022-02-16 08:48:48 -0300

    Adicionar arquivos de projeto.

---
+++ Fatorial.cs
@@ -0,0 +1,19 @@
+﻿public class Fatorial
+{
+    static int FatorialRecursivo(int Numero)
+    {
+        if (Numero > 1) return (Numero * FatorialRecursivo(Numero - 1));
+        return Numero;
+    }
+
+    public static void Main(string[] args)
+    {
+        int Numero = 1;
+        while (Numero != 0)
+        {
+            Console.Write("Informe um número entre 1 e 10 para calcular o fatorial (0 para sair): ");
+            Numero  = int.Parse(Console.ReadLine());
+            if (Numero > 0 & Numero < 11)Console.Write($"{Numero}! = {FatorialRecursivo(Numero)}\n\n\n");
+        }
+    }
+}
